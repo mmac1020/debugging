819347cdfb8d05fd38169996c5f3a3b0117b0a9c
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2022-03-02 10:56:10 -0500

    Adding code from class

--- detection-and-analysis/detection.js
+++ detection-and-analysis/detection.js
@@ -1,37 +1,31 @@
-// let add = (a, b) => {
-//   return a + b;
-// };
+const add = (a, b) => a + b;
 
-// console.log('logging add', add(10, 20));
+console.log('logging add', add(10, 20));
 
-// let mac = {
-//   name: 'mac',
-//   instructor: true
-// };
+const mac = {
+  name: 'mac',
+  instructor: true,
+};
 
-// console.log(mac);
+console.log(mac);
 
-// function nested() {
-//   function nestedAgain() {
-//     function anotherNest() {
-//       function aLotOfBirdsHere() {
-//         console.trace('nested trace');
-//       }
-//       aLotOfBirdsHere();
-//     }
-//     anotherNest();
-//   }
-//   nestedAgain();
-// }
-
-// nested();
-
-let x = 0;
-
-for (var i = 0; i < 10; i++) {
-  console.log('some context ', i);
-  console.count('some context');
-  i = 10;
+function nested() {
+  function nestedAgain() {
+    function anotherNest() {
+      function aLotOfBirdsHere() {
+        console.trace('my stack trace log');
+      }
+      aLotOfBirdsHere();
+    }
+    anotherNest();
+  }
+  nestedAgain();
 }
 
-console.count('outside the loop');
+nested();
+
+const x = 0;
+
+for (let i = 0; i < 100; i++) {
+  console.count('to make it obvious that I am console counting');
+}
--- prevention/prevention.js
+++ prevention/prevention.js
@@ -2,11 +2,14 @@ const foo = 'foo';
 
 function add(a, b) {
   return a + b;
+
+  console.log('hello');
 }
 function nestedFunctions() {
   function anotherNest() {
     const c = 10;
-    function anotherNestNEst() {}
+
+    function anotherNestNest() {}
   }
 }
 
